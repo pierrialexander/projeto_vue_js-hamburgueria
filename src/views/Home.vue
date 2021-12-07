5ea6f8d518162875ea210896e750be4e3a3ed2e7
Pierri Alexander Vidmar <pierrialexandervidmar@gmail.com>
2021-12-06 22:14:20 -0300

    Criado banner da aplicação
    Criado banner da aplicação

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,19 @@
 <template>
+  <Banner/>
   <div class="main-container">
-    <h1>HOME</h1>
+    <h1>Monte seu burger:</h1>
+    <p>formulario</p>
   </div>
 </template>
 
-<script></script>
+<script>
+  import Banner from "../components/Banner.vue"
+
+  export default {
+    name: "Homme",
+    components: {
+      Banner
+    }
+  }
+
+</script>
